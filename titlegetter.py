09a54ce40c33d93d09ceb3d9ea830225194bbfe4
WeepingDogel <sydghbsdfyghsdy456@gmail.com>
2020-11-26 14:03:34 +0800

    面向对象化重构

--- titlegetter.py
+++ titlegetter.py
@@ -4,108 +4,124 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import toml
-def print_as_markdown(a,b):
-    print("["+a+"]"+"("+b+")")
-# load config
-config = toml.load('config.toml')
-# check Language config
-if config['Language']['lang'] == "en-US":
-    input_tip = "Please type an URL: "
-    version = "Version: "
-    requests_success = "Requested Successfully"
-    requests_failure = "Failed to request"
-    result = "Title: "
-    filename_input = "Type a filename: "
-    empty_line_warning = "WARN: empty line is forbidden."
-elif config['Language']['lang'] == "zh-CN":
-    input_tip = "请输入一个链接: "
-    version = "版本："
-    requests_success = "请求成功"
-    requests_failure = "请求失败"
-    result = "标题: "
-    filename_input = "输入文件名: "
-    empty_line_warning = "警告：禁止空行"
-# sign of program
-def sign():
-    print('''
-╭━━━━╮╭╮╭╮╱╱╱╱╭━━━╮╱╱╭╮╱╭╮
-┃╭╮╭╮┣╯╰┫┃╱╱╱╱┃╭━╮┃╱╭╯╰┳╯╰╮
-╰╯┃┃┣╋╮╭┫┃╭━━╮┃┃╱╰╋━┻╮╭┻╮╭╋━━┳━╮
-╱╱┃┃┣┫┃┃┃┃┃┃━┫┃┃╭━┫┃━┫┃╱┃┃┃┃━┫╭╯
-╱╱┃┃┃┃┃╰┫╰┫┃━┫┃╰┻━┃┃━┫╰╮┃╰┫┃━┫┃
-╱╱╰╯╰╯╰━┻━┻━━╯╰━━━┻━━┻━╯╰━┻━━┻╯
-    ''')
-    print(version + "1.0.1\n")
-sign()
-# create a session of requesting.
-session = requests.session()
-# import the headers
-headers = config['headers']
-# check the working mode
+## classes
+class Interactions:
+    def ckbatch(self,config):
+        if config['BatchMode']['Turn_on'] == False:
+            print("Batch OFF")
+            return 0
+        elif config['BatchMode']['Turn_on'] == True:
+            print("Batch ON")
+            return 1
+    def cklang(self,config):
+        if config['Language']['lang'] == "en_US":
+            return 0
+        if config['Language']['lang'] == "zh_CN":
+            return 1
+    def sign(self,config):
+        print(config['Sign']['LOGO'])
+    def show_version(self,config,lang):
+        if lang == 0:
+            print(config['Language']['en_US']['version'] + config['Sign']['Version'])
+        if lang == 1:
+            print(config['Language']['zh_CN']['version'] + config['Sign']['Version'])
+    def getURL(self,lang):
+        if lang == 0:
+            URL = input(config['Language']['en_US']['input_tip'])
+        if lang == 1:
+            URL = input(config['Language']['zh_CN']['input_tip'])
+        return URL
+    def output_asking(self,config,lang):
+        if lang == 0:
+            print(config['Language']['en_US']['output_asking'])
+        if lang == 1:
+            print(config['Language']['zh_CN']['output_asking'])
+        print('''
+(0) Pure TEXT
+(1) MarkDown
+(2) HTML
+                ''')
+        choose = int(input(":"))
+        return choose
+class Main:
+    def loadconfig(self,filename):
+        config = toml.load(filename)
+        return config
+    def getPage(self,headers,URL):
+        session = requests.session()
+        response = session.get(URL,headers=headers)
+        if response.status_code == 200:
+            print("200 OK")
+            return response.text
+        else:
+            print(URL + "Get Page Failed")
+            os._exit(0)
+class Process:
+    def getTitle(self,Page):
+        soup = BeautifulSoup(Page,'lxml')
+        title = soup.find('title')
+        return title.string.strip()
+    def print_as_pure_text(self,title,URL):
+        print('-' * 20)
+        print("Title:" + title)
+        print("URL:" + URL)
+    def print_as_markdown(self,title,URL):
+        print("-" * 20)
+        print("["+title+"]"+"("+URL+")")
+    def print_as_html(self,title,URL):
+        print("-" * 20)
+        print("<a href=" + "\"" + URL + "\"" + ">" + title + "</a>")
+    def get_filename(self, lang, config):
+        if lang == 0:
+            filename = input(config['Language']['en_US']['filename_input'])
+        elif lang == 1:
+            filename = input(config['Language']['zh_CN']['filename_input'])
+        return filename
+## classes
 
-if config['BatchMode'] == True:
-    filename = input(filename_input)
-    if config['AsMarkDown'] == True:
-        file_format = ".md"
-    else:
-        file_format = ".txt"
-    # Batch Mode
+## Running
+A = Main()
+B = Process()
+config = A.loadconfig('config.toml')
+C = Interactions()
+C.sign(config=config)
+lang = C.cklang(config=config)
+C.show_version(config=config,lang=lang)
+if C.ckbatch(config=config) == 0:
+    URL = C.getURL(lang=lang)
+    page = A.getPage(headers=config['headers'],URL=URL)
+    choose = C.output_asking(config=config,lang=lang)
+    title = B.getTitle(page)
+    if choose == 0:
+        B.print_as_pure_text(title=title,URL=URL)
+    if choose == 1:
+        B.print_as_markdown(title=title,URL=URL)
+    if choose == 2:
+        B.print_as_html(title=title,URL=URL)
+elif C.ckbatch(config=config) == 1:
+    def ck_format():
+        choose = C.output_asking(config=config,lang=lang)
+        if choose == 0:
+            file_format = '.txt'
+        if choose == 1:
+            file_format = '.md'
+        if choose == 2:
+            file_format = '.html'
+        return file_format
+    filename = B.get_filename(lang=lang,config=config)
+    file_format = ck_format()
     with open('out/'+filename+ file_format,'x',encoding="utf8") as f:
-         # load the URLs
-        urlist = open('list.txt')
-        for x in urlist:
-            # .strip() : delete the blank line
-            url = x.strip()
-            # empty line checking
-            if url == '':
-                print(empty_line_warning)
-                urlist.close()
-                os.remove("./out/" + filename + file_format)
-                os._exit(0)
-                # empty line checking
-            response = session.get(url,headers=headers)
-            if response.status_code == 200:
-                print(requests_success)
-            else:
-                print(requests_failure)
-            if config['EncodingFix'] == True:
-                # check if the EncodingFix is true
-                source = response.text.encode('ISO8859-1')
-            else:
-                source = response.text
-            soup = BeautifulSoup(source,'lxml')
-            a = soup.find('title')
-            titles = a.string.strip()
-            if config['AsMarkDown'] == True:
-                f.write("* ["+titles+"]"+"("+url+")\n\n")
-                print_as_markdown(titles,url)
-            else:
-                f.write(result+titles + "\n" + "URL: " + url + "\n\n")
-                print(result + titles)
-                print("URL: " +  url)
-                print("-" * 20)
-else:
-    # not on batch mode
-    # request the URL
-    url = input(input_tip)
-    response = session.get(url,headers=headers)
-    if response.status_code == 200:
-        print(requests_success)
-        # parse the web source
-        if config['EncodingFix'] == True:
-            # check if the EncodingFix is true
-            source = response.text.encode('ISO8859-1')
-        else:
-            source = response.text
-        soup = BeautifulSoup(source,'lxml')
-        title = soup.find('title') # get the title from the web source
-        # check the handling mode
-        if config['AsMarkDown'] == True:
-            print(result)
-            print_as_markdown(title.string.strip(),url)
-        else:
-            print(result + title.string.strip())
-            print("URL: " + url)
-    else:
-        # end
-        print(requests_failure)
+        URLIST = open('list.txt')
+        for URL in URLIST:
+            pure_url = URL.strip()
+            page = A.getPage(headers=config['headers'], URL=pure_url)
+            title = B.getTitle(page)
+            if file_format == '.txt':
+                f.write('Title:' + title + '\n' + 'URL:' + pure_url + '\n\n')
+                B.print_as_pure_text(title=title, URL=pure_url)
+            elif file_format == '.md':
+                f.write("["+title+"]"+"("+pure_url+")\n\n")
+                B.print_as_markdown(title=title,URL=pure_url)
+            elif file_format == '.html':
+                f.write("<a href=" + "\"" + pure_url + "\"" + ">" + title + "</a>" + "\n" + "<br>" + "\n")
+                B.print_as_html(title,URL=pure_url)
